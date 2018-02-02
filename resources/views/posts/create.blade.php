14c2fd887880132ee50fa192decc2354d127b9ec
wangxis <wxsh55@gmail.com>
2018-02-01 23:01:29 -0800

    validation

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -9,20 +9,23 @@
 <form method="POST" action="/home/public/posts">
 	{{ csrf_field()}}
 
-	<div class="form-group">
-		<label for="title">Title</label>
-		<input type="text" class="form-control" id="title" name="title">
-	</div>
+  	<div class="form-group">
+  		<label for="title">Title</label>
+  		<input type="text" class="form-control" id="title" name="title">
+  	</div>
 
-	<div class="form-group">
-		<label for="body">Body:</label>
-		<textarea id="body" name="body" class="form-control"></textarea>
-	</div>
-
-	<button type="submit" class="btn btn-default">Publish</button>
-
+  	<div class="form-group">
+  		<label for="body">Body:</label>
+  		<textarea id="body" name="body" class="form-control"></textarea>
+  	</div>
+    <div class="form-group">
+      
+ <button type="submit" class="btn btn-primary">Publish</button>
+    </div>
+	
+   @include('layouts.errors')
 </form>
-</div> 
+  </div>
 @endsection
 
 
---
+++ resources/views/posts/post.blade.php
@@ -0,0 +1,6 @@
+<div class="blog-post">
+	<h2 class="blog-post-title"><a href="/home/public/posts/{{ $post->id }}">{{ $post->title }}</a></h2>
+	<p class="blog-post-meta">{{ $post->created_at->toFormattedDateString() }} </p>
+	{{ $post->body }}
+
+</div>
